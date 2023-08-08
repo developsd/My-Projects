40d4916e29fc3d66af16b992544944a23c19ade7
Samar Dixit <137815277+developsd@users.noreply.github.com>
2023-08-08 12:14:06 +0530

    portfolio

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Samar Dixit - Portfolio</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+    <header>
+          <div class="profile-info">
+                <h1>Samar Dixit</h1>
+                <p>From Kushinagar, Uttar Pradesh (274402)</p>
+                <p>Email: samardixit515@gmail.com</p>
+                <p>Phone: 9335434866 / 8795592402</p>
+           </div>
+            <img class="profile-photo" src="samar.jpg" alt="Samar Dixit">
+    </header>
+        <section>
+            <h2>Objective</h2>
+            <p>A highly motivated and detail-oriented B.Tech Computer Science graduate with a passion for Back-end development. Seeking a challenging role as a Fresher Back-End Developer to leverage my skills in HTML, CSS, and JavaScript to create visually appealing and user-friendly web applications.</p>
+        </section>
+        <section>
+            <h2>Education</h2>
+            <p>Bachelor of Technology in Computer Science and Engineering</p>
+            <p>Neelkanth Institute of Technology (NIT), Meerut</p><br>
+            <p>12th (Mathematics)</p>
+            <p>Government Higher Secondary School • Deoria, Uttar Pradesh</p><br>
+            <p>Metric (Science)</p>
+            <p>Government Higher Secondary School • Kushinagar, Uttar Pradesh</p>
+        </section>
+
+        <section>
+            <h2>Achievements</h2>
+            <p>Outstanding Achievement Award in College Activity</p>
+        </section>
+
+        <section>
+            <h2>Internship</h2>
+            <p>Java Internship at AppSquadz Education, Noida</p>
+        </section>
+
+        <section>
+            <h2>Projects</h2>
+            <ul class="projects-list">
+                <li>
+                    <strong>Portfolio</strong>
+                    <ul>
+                        <li>Developed a responsive website for an online portfolio using HTML, CSS, and JavaScript.</li>
+                        <li>Implemented smooth animations and interactive features to enhance user experience.</li>
+                        <li>Hosted the project on GitHub Pages for easy accessibility.</li>
+                        <li>GitHub Link: <a href="https://github.com/your_username/portfolio" target="_blank">View on GitHub</a></li> <!-- Replace "your_username" with your actual GitHub username and update the link to match your repository URL -->
+                    </ul>
+                </li>
+                <li>
+                    <strong>Weather Forecast</strong>
+                    <ul>
+                        <li>Collaborated with a team to create a weather forecast web application.</li>
+                        <li>Utilized third-party APIs to fetch real-time weather data and display it on the interface.</li>
+                        <li>Implemented dynamic updates based on user input using JavaScript.</li>
+                        <li>GitHub Link: <a href="https://github.com/your_username/weather-forecast" target="_blank">View on GitHub</a></li> <!-- Replace "your_username" with your actual GitHub username and update the link to match your repository URL -->
+                    </ul>
+                </li>
+                <!-- Add more projects here -->
+            </ul>
+        </section>
+
+        <section>
+            <h2>Skills</h2>
+            <ul>
+                <li>Java</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Php</li>
+                <li>Problem-solving</li>
+                <!-- Add more skills here -->
+            </ul>
+        </section>
+        <section>
+            <h2>Languages</h2>
+            <ul class="languages-list">
+                <li>English</li>
+                <li>Hindi</li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Hobbies</h2>
+            <ul class="hobbies-list">
+                <li>Travelling</li>
+                <li>Problem Solving</li>
+            </ul>
+        </section>
+
+        <footer>
+            <p>Contact me at: samardixit515@gmail.com</p>
+            <p>&copy; <?php echo date("Y"); ?> Samar Dixit - Portfolio. All rights reserved.</p>
+        </footer>
+    </div>
+</body>
+</html>
